0ba9aebe67f070cf36d8c10b133a76b9a90272fa
gryaznykh-ivan <gryaznykh-ivan@gmail.com>
2023-08-19 17:48:10 +0300

    editor resize

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,6 +1,17 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  HostListener,
+  OnInit,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { StageConfig } from 'konva/lib/Stage';
 import { CircleConfig } from 'konva/lib/shapes/Circle';
+import { debounceTime, fromEvent } from 'rxjs';
 import { CoreShapeComponent } from 'src/app/libs/ng2-konva/components/core-shape.component';
 import { StageComponent } from 'src/app/libs/ng2-konva/components/stage.component';
 
@@ -9,13 +20,15 @@ import { StageComponent } from 'src/app/libs/ng2-konva/components/stage.componen
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   imports: [StageComponent, CoreShapeComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EditorComponent {
-  public configStage: Partial<StageConfig> = {
-    width: 200,
-    height: 500,
+export class EditorComponent implements OnInit, AfterViewInit {
+  stage: Partial<StageConfig> = {
+    width: 100,
+    height: 100,
   };
-  public configCircle: CircleConfig = {
+
+  circle: CircleConfig = {
     x: 100,
     y: 100,
     radius: 70,
@@ -23,4 +36,29 @@ export class EditorComponent {
     stroke: 'black',
     strokeWidth: 4,
   };
+
+  constructor(private descroyRef: DestroyRef, private cd: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    fromEvent(window, 'resize')
+      .pipe(takeUntilDestroyed(this.descroyRef), debounceTime(10))
+      .subscribe(this.onResize);
+  }
+
+  ngAfterViewInit(): void {
+    this.onResize();
+  }
+
+  onResize = () => {
+    const container =
+      document.querySelector<HTMLDivElement>('#editor-container');
+    if (container === null) return;
+
+    this.stage = {
+      width: container.offsetWidth,
+      height: container.offsetHeight,
+    };
+
+    this.cd.detectChanges()
+  }
 }
